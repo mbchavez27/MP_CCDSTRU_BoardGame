7c67d9959bbd52e0de98c3a0c742c4afe692de56
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-03-02 13:18:52 +0800

    feat: add game board

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#define A_SET 4
+
+void gameBoard() {
+  for (int i = 0; i < A_SET; i++) {
+    for (int j = 0; j < A_SET; j++) {
+      printf("| %c | ", 'x');
+    }
+    printf("\n");
+  }
+}
+
+int main() {
+
+  char P_board[A_SET][A_SET];
+
+  gameBoard();
+
+  return 0;
+}
