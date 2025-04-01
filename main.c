1c8f2185e9c768ddac081dbb7df9e9d883a20c55
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-04-02 00:02:04 +0800

    feat: finished

--- main.c
+++ main.c
@@ -6,6 +6,66 @@
 #define A_SET 4
 typedef char String25[26];
 
+/************************************************************************************************************************
+This is to certify that this project is my own work, based on my personal efforts in studying and applying the concepts
+learned.  I have constructed the functions and their respective algorithms and corresponding code by myself.  The
+program was run, tested, and debugged by my own efforts.  I further certify that I have not copied in part or whole or
+otherwise plagiarized the work of other students and/or persons.
+
+                                                                              Max Benedict B. Chavez,    DLSU ID 12328146
+************************************************************************************************************************/
+
+void printInstructions()
+{
+  const char *red = "\033[1;31m";
+  const char *green = "\033[1;32m";
+  const char *reset = "\033[0m";
+
+  printf("%s+---------------------------+%s\n", red, reset);
+  printf("%s|   4 STEPS INSTRUCTIONS    |%s\n", red, reset);
+  printf("%s+---------------------------+%s\n", red, reset);
+  printf("%s| 1. Use 'W', 'A', 'S', 'D'  |%s\n", green, reset);
+  printf("%s|    to move around the grid |%s\n", green, reset);
+  printf("%s| 2. Press 'Q' to confirm    |%s\n", green, reset);
+  printf("%s| 3. Place your mark (T/U)   |%s\n", green, reset);
+  printf("%s|    on the board            |%s\n", green, reset);
+  printf("%s| 4. Try to complete a row,  |%s\n", green, reset);
+  printf("%s|    column, or diagonal!    |%s\n", green, reset);
+  printf("%s+---------------------------+%s\n", red, reset);
+  printf("\n\n");
+  fgetc(stdin);
+}
+
+void printTitle()
+{
+  const char *red = "\033[1;31m";
+  const char *green = "\033[1;32m";
+  const char *reset = "\033[0m";
+
+  printf("%s _____                ____              _   _             %s\n", red, reset);
+  printf("|_   _| __ ___  ___  |  _ \\  ___  ___  | | | |_ __   ___  %s\n", green);
+  printf("  | || '__/ _ \\/ __| | | | |/ _ \\/ __| | | | | '_ \\ / _ \\ %s\n", red);
+  printf("  | || | |  __/\\__ \\ | |_| | (_) \\__ \\ | |_| | | | | (_) | %s\n", green);
+  printf("  |_||_|  \\___||___/ |____/ \\___/|___/  \\___/|_| |_|\\___/  %s\n", red);
+  printf("%s", reset);
+  printf("\n");
+}
+
+void printMenu()
+{
+  const char *red = "\033[1;31m";
+  const char *green = "\033[1;32m";
+  const char *reset = "\033[0m";
+
+  printf("%s+---------------------+%s\n", red, reset);
+  printf("%s|     MENU OPTIONS    |%s\n", red, reset);
+  printf("%s+---------------------+%s\n", red, reset);
+  printf("%s| 1. Play             |%s\n", green, reset);
+  printf("%s| 2. Instructions     |%s\n", green, reset);
+  printf("%s| 3. Exit             |%s\n", green, reset);
+  printf("%s+---------------------+%s\n", red, reset);
+}
+
 void checkWinner(char P_board[][A_SET], bool *gameOver, String25 winner)
 {
   // Tres
@@ -89,6 +149,7 @@ void printBoard(char P_board[][A_SET], bool isUnoTurn, bool isDosTurn,
                 bool isTresTurn, int row, int col)
 {
   system("clear");
+  printTitle();
   printf("Board: ");
 
   if (isTresTurn)
@@ -123,6 +184,33 @@ void initBoard(char P_board[][A_SET])
   }
 }
 
+void nextPlayerMove(char P_board[][A_SET], bool *isUnoTurn, bool *isDosTurn,
+                    bool *isTresTurn, int row, int col, bool *turn)
+{
+  *turn = false;
+  if (*isTresTurn && P_board[row][col] == '-')
+  {
+    P_board[row][col] = 'T';
+    *isTresTurn = false;
+    *isUnoTurn = true;
+    *isDosTurn = false;
+  }
+  else if (*isUnoTurn && P_board[row][col] == '-')
+  {
+    P_board[row][col] = 'U';
+    *isTresTurn = false;
+    *isUnoTurn = false;
+    *isDosTurn = true;
+  }
+  else if (*isDosTurn)
+  {
+    P_board[row][col] = ' ';
+    *isTresTurn = true;
+    *isUnoTurn = false;
+    *isDosTurn = false;
+  }
+}
+
 void gameTurn(char P_board[][A_SET], bool *isUnoTurn, bool *isDosTurn,
               bool *isTresTurn)
 {
@@ -148,7 +236,7 @@ void gameTurn(char P_board[][A_SET], bool *isUnoTurn, bool *isDosTurn,
   while (turn)
   {
     printBoard(P_board, *isUnoTurn, *isDosTurn, *isTresTurn, row, col);
-    printf("(%d,%d): ", row, col);
+    printf("(%d,%d): ", row + 1, col + 1);
     ch = getchar();
     while (getchar() != '\n')
       ;
@@ -187,28 +275,7 @@ void gameTurn(char P_board[][A_SET], bool *isUnoTurn, bool *isDosTurn,
       break;
     case 'Q':
     case 'q':
-      turn = false;
-      if (*isTresTurn && P_board[row][col] == '-')
-      {
-        P_board[row][col] = 'T';
-        *isTresTurn = false;
-        *isUnoTurn = true;
-        *isDosTurn = false;
-      }
-      else if (*isUnoTurn && P_board[row][col] == '-')
-      {
-        P_board[row][col] = 'U';
-        *isTresTurn = false;
-        *isUnoTurn = false;
-        *isDosTurn = true;
-      }
-      else if (*isDosTurn)
-      {
-        P_board[row][col] = ' ';
-        *isTresTurn = true;
-        *isUnoTurn = false;
-        *isDosTurn = false;
-      }
+      nextPlayerMove(P_board, isUnoTurn, isDosTurn, isTresTurn, row, col, &turn);
       break;
     }
     if (P_board[prevRow][prevCol] == '-')
@@ -222,20 +289,44 @@ void gameTurn(char P_board[][A_SET], bool *isUnoTurn, bool *isDosTurn,
 
 int main()
 {
-  char P_board[A_SET][A_SET];
-  bool isUnoTurn = false, isDosTurn = false, isTresTurn = true;
-  bool gameOver = false;
-  String25 winner;
-
-  // Set Board
-  initBoard(P_board);
-
-  while (!gameOver)
+  int input;
+  while (input != 3)
   {
-    gameTurn(P_board, &isUnoTurn, &isDosTurn, &isTresTurn);
-    checkWinner(P_board, &gameOver, winner);
+    printTitle();
+    printMenu();
+
+    printf("Input: ");
+    scanf("%d", &input);
+
+    if (input == 1)
+    {
+      input = -1;
+      char P_board[A_SET][A_SET];
+      bool isUnoTurn = false, isDosTurn = false, isTresTurn = true;
+      bool gameOver = false;
+      String25 winner;
+
+      // Set Board
+      initBoard(P_board);
+
+      while (!gameOver)
+      {
+        gameTurn(P_board, &isUnoTurn, &isDosTurn, &isTresTurn);
+        checkWinner(P_board, &gameOver, winner);
+      }
+      printf("\n\n");
+      printf("Game Over!!!!\n");
+      printf("Winner is %s\n", winner);
+      printf("\n\n");
+    }
+    if (input == 2)
+    {
+      printInstructions();
+    }
+    if (input == 3)
+    {
+      printf("Goodbye\n");
+    }
   }
-  printf("Game Over!!!!\n");
-  printf("Winner is %s\n", winner);
   return 0;
 }
